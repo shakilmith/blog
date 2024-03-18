c08d6b493dfc0d6a7fe87277d96dc115c5088d0b
shakilmith <shakilmith@gmail.com>
2024-03-18 11:35:52 +0600

    new commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   "gatsby-plugin-sharp", 
   "gatsby-transformer-sharp", 
   "gatsby-transformer-remark",
+  "gatsby-plugin-decap-cms",
 
   {
     resolve: `gatsby-plugin-mdx`,
